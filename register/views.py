69b04305a152a76b205940d84849ab6c01ef4411
MrSpeed30 <mr.speedman174@gmail.com>
2026-03-10 12:55:41 +0300

    update for register, register.views

--- register/views.py
+++ register/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-    form = UserCreationForm()
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.clean_username.get('username')
+    else:
+        form = UserCreationForm()
     return render(request,'register\\register.html', {'form': form})
