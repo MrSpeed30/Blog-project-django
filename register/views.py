a39cef90a1c66dbb63f36c1aa1939d31a06b153b
MrSpeed30 <mr.speedman174@gmail.com>
2026-03-12 14:05:50 +0300

    updated reg thingy

--- register/views.py
+++ register/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
 def register(request):
     if request.method =='POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            username = form.clean_username.get('username')
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome to the blog, {username}!')
+            return redirect('Blog_home')
     else:
         form = UserCreationForm()
-    return render(request,'register\\register.html', {'form': form})
+    return render(request,'register\\register.html', {'form': form})        
